a8ba1e8680cbca421b6449f0adb504b3393d0255
bizsystemteax <bizsystemtax@naver.com>
2024-07-23 16:19:54 +0900

    20240723_commit

--- src/main/java/egovframework/com/security/SecurityConfig.java
+++ src/main/java/egovframework/com/security/SecurityConfig.java
@@ -40,6 +40,7 @@ public class SecurityConfig {
         "/file",
         "/excel/upload",
         "/excel/download",
+        "/excel/search",
         "/v3/api-docs/**",
         "/swagger-resources",
         "/swagger-resources/**",
