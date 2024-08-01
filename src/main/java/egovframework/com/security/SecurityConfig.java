933f1c00f763873d67e9d159b27088a72aae669b
bizsystemteax <bizsystemtax@naver.com>
2024-08-01 17:49:14 +0900

    20240801

---
+++ src/main/java/egovframework/com/security/SecurityConfig.java
@@ -0,0 +1,92 @@
+package egovframework.com.security;
+
+import egovframework.com.jwt.JwtAuthenticationEntryPoint;
+import egovframework.com.jwt.JwtAuthenticationFilter;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig {
+
+    private String[] AUTH_GET_WHITELIST = {
+        "/mainPage",
+        "/board",
+        "/board/{bbsId}/{nttId}",
+        "/boardFileAtch/{bbsId}",
+        "/schedule/daily",
+        "/schedule/week",
+        "/schedule/{schdulId}",
+        "/image"
+    };
+
+    private String[] AUTH_WHITELIST = {
+        "/",
+        "/login/**",
+        "/auth/login-jwt",
+        "/auth/login",
+        "/file",
+        "/excel/upload",
+        "/excel/download",
+        "/code/commonCode",
+        "/excel/search",
+        "/penaltyStd/comboBoxList",
+        "/penaltyStd/list",
+        "/v3/api-docs/**",
+        "/swagger-resources",
+        "/swagger-resources/**",
+        "/swagger-ui.html",
+        "/swagger-ui/**"
+    };
+
+    @Bean
+    public JwtAuthenticationFilter authenticationTokenFilterBean() throws Exception {
+        return new JwtAuthenticationFilter();
+    }
+
+    @Bean
+    protected CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        configuration.setAllowedOrigins(Arrays.asList("http://localhost:3000")); // 특정 도메인만 허용
+        configuration.setAllowedMethods(Arrays.asList("HEAD", "POST", "GET", "DELETE", "PUT", "PATCH")); // 허용할 HTTP 메서드
+        configuration.setAllowedHeaders(Arrays.asList("*")); // 모든 헤더 허용
+        configuration.setAllowCredentials(true); // 자격 증명 허용
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
+
+    @Bean
+    protected SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors().configurationSource(corsConfigurationSource()).and()
+            .authorizeHttpRequests(authorize -> authorize
+                .antMatchers(AUTH_WHITELIST).permitAll()
+                .antMatchers(HttpMethod.GET, AUTH_GET_WHITELIST).permitAll()
+                .anyRequest().authenticated()
+            )
+            .sessionManagement(sessionManagement -> 
+                sessionManagement.sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+            )
+            .addFilterBefore(authenticationTokenFilterBean(), UsernamePasswordAuthenticationFilter.class)
+            .exceptionHandling(exceptionHandlingConfigurer -> 
+                exceptionHandlingConfigurer
+                    .authenticationEntryPoint(new JwtAuthenticationEntryPoint())
+            )
+            .build();
+    }
+}
