0edd2f5d49079105dbbca15a5c3a22b92f9614c4
bizsystemtax <bizsystemtax@naver.com>
2025-09-11 15:15:57 +0900

    bizsystemtax test

--- src/main/java/egovframework/penalty/service/FineMngeService.java
+++ src/main/java/egovframework/penalty/service/FineMngeService.java
@@ -8,7 +8,7 @@ import egovframework.penalty.FineMngeVO;
 
 
 public interface FineMngeService {
-	//범칙금관리 검색조건 콤보박스 조회 테스트
+	//범칙금관리 검색조건 콤보박스 조회
 	public Map<String, Object> retrieveComboBoxList(ComnCdVO comnCdVO) throws Exception;
 
 	//범칙금관리 목록 조회
