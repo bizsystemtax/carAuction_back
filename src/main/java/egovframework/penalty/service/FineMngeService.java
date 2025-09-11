a8850ae1323e10a73bb653907c775cf45dec4653
dalsejin <dalsejin@gmail.com>
2025-09-11 15:04:34 +0900

    dalsejin test

--- src/main/java/egovframework/penalty/service/FineMngeService.java
+++ src/main/java/egovframework/penalty/service/FineMngeService.java
@@ -8,7 +8,7 @@ import egovframework.penalty.FineMngeVO;
 
 
 public interface FineMngeService {
-	//범칙금관리 검색조건 콤보박스 조회 테스트입니다...
+	//범칙금관리 검색조건 콤보박스 조회 테스트
 	public Map<String, Object> retrieveComboBoxList(ComnCdVO comnCdVO) throws Exception;
 
 	//범칙금관리 목록 조회
