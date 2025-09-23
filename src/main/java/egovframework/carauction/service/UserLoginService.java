d0e556ed8fec23d18e387a65b4cc94fb21214949
Kimdanieru <kdno1118@naver.com>
2025-09-23 17:34:19 +0900

    25.09.23 danieru...

--- src/main/java/egovframework/carauction/service/UserLoginService.java
+++ src/main/java/egovframework/carauction/service/UserLoginService.java
@@ -3,80 +3,12 @@ package egovframework.carauction.service;
 import java.util.List;
 import java.util.Map;
 
-import egovframework.carauction.CarInfoVO;
-import egovframework.carauction.CarSearchCriteriaVO;
 
 /**
  * 차량 경매 정보 / 차량 판매 정보 서비스 인터페이스
  */
 public interface UserLoginService {
 
-	/************************************************************************************************************************
-	 차량 경매 정보
-	************************************************************************************************************************/
 	
-    /**
-     * 검색 조건에 따른 차량 목록 조회
-     * @param criteria 검색 조건
-     * @return 차량 목록
-     * @throws Exception
-     */
-    List<CarInfoVO> findCarsWithConditions(CarSearchCriteriaVO criteria) throws Exception;
-
-    /**
-     * 제조사 목록 조회
-     * @return 제조사 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findAllManufacturers() throws Exception;
-
-    /**
-     * 전체 모델 목록 조회
-     * @return 모델 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findAllModels() throws Exception;
-
-    /**
-     * 제조사별 모델 목록 조회
-     * @param manufacturer 제조사명
-     * @return 모델 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findModelsByManufacturer(String manufacturer) throws Exception;
-
-    /**
-     * 전체 세부모델 목록 조회
-     * @return 세부모델 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findAllSubModels() throws Exception;
-
-    /**
-     * 제조사 및 모델별 세부모델 목록 조회
-     * @param manufacturer 제조사명
-     * @param model 모델명
-     * @return 세부모델 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findSubModelsByManufacturerAndModel(String manufacturer, String model) throws Exception;
-
-    /**
-     * 연료타입 목록 조회
-     * @return 연료타입 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findAllFuelTypes() throws Exception;
-
-    /**
-     * 등록회원사 목록 조회
-     * @return 등록회원사 목록
-     * @throws Exception
-     */
-    List<Map<String, Object>> findAllRegistrationCompanies() throws Exception;
-    
-    /************************************************************************************************************************
-	 차량 판매 정보
-	************************************************************************************************************************/
     
 }
--- src/main/java/egovframework/carauction/service/impl/UserLoginDAO.java
+++ src/main/java/egovframework/carauction/service/impl/UserLoginDAO.java
@@ -5,23 +5,9 @@ import java.util.List;
 import org.egovframe.rte.psl.dataaccess.EgovAbstractMapper;
 import org.springframework.stereotype.Repository;
 
-import egovframework.carauction.MyPageVO;
 
 @Repository("UserLoginDAO")
 public class UserLoginDAO extends EgovAbstractMapper {
 	
-	//콤보박스 진행상태 조회
-	public List<MyPageVO> comboBoxList(MyPageVO myPageVO) {
-		return selectList("MyPageDAO.comboBoxList", myPageVO);
-	}
-	
-	//마이페이지 - 내 판매차량 입찰 상세 현황
-	public List<MyPageVO> myPageList(MyPageVO myPageVO) {
-		return selectList("MyPageDAO.myPageList", myPageVO);
-	}
 
-	//마이페이지 - 내 판매차량 입찰 상세 현황(경매(공매) 등록내용)
-	public List<MyPageVO> mySaleCarAuctionRegList(MyPageVO myPageVO) {
-		return selectList("MyPageDAO.mySaleCarAuctionRegList", myPageVO);
-	}
 }
