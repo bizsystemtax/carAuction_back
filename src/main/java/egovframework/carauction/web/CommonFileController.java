086af5d8be772930c42a6a7d06169b96e8289eec
scpark0502 <sctyaos@naver.com>
2025-10-21 10:11:07 +0900

    psc - 첨부파일 삭제 처리 로직 구현

---
+++ src/main/java/egovframework/carauction/web/CommonFileController.java
@@ -0,0 +1,70 @@
+package egovframework.carauction.web;
+import java.io.FileNotFoundException;
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
+import javax.transaction.Transactional;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import egovframework.carauction.AttachFileVO;
+import egovframework.carauction.service.CommonFileService;
+import egovframework.com.cmm.service.ResultVO;
+
+@RestController
+@Transactional
+@RequestMapping("/file")
+public class CommonFileController {
+	
+	@Value("${file.upload.path}")
+	private String uploadRootPath;
+	
+	
+	private static final Logger logger = LoggerFactory.getLogger(CommonFileController.class);
+
+	@Resource(name = "commonFileService")
+	private CommonFileService commonFileService; 
+
+	
+	//파일 삭제 
+	@PostMapping("/fileDelete")
+    public ResultVO deleteFileInf(
+    		HttpServletRequest request, 
+    		@RequestBody AttachFileVO attachFileVO
+    		) throws Exception {
+    	ResultVO resultVO = new ResultVO();
+		
+    	String atchFileId = attachFileVO.getAttFileId().replaceAll(" ", "+");
+    	//byte[] decodedBytes = Base64.getDecoder().decode(atchFileId);
+    	//String decodedFileId = new String(decodedBytes, StandardCharsets.UTF_8);
+    	
+    	logger.info("request      :::::::::::::: {}", request);
+    	logger.info("attachFileVO :::::::::::::: {}", attachFileVO);
+    	logger.info("삭제가 되는지 확인  :::::::::::::: {}", atchFileId);
+    	//logger.info("decodedBytes :::::::::::::: {}", decodedBytes);
+    	//logger.info("decodedFileId :::::::::::::: {}", decodedFileId);
+    	
+    	try {
+    		commonFileService.deleteFile(attachFileVO);
+    		resultVO.setResultCode(200);
+			resultVO.setResultMessage("삭제 성공");
+    	}catch (FileNotFoundException e) {
+    		resultVO.setResultCode(404);
+			resultVO.setResultMessage("파일이 존재하지 않습니다.");
+		}catch (Exception e) {
+			resultVO.setResultCode(500);
+			resultVO.setResultMessage("파일 삭제 실패");
+		}
+		
+		return resultVO;
+	}
+}
