8f5dfa0174f5a08686a97eef60a9a94f67c6cd40
bizsystemtax <bizsystemtax@naver.com>
2025-10-30 14:18:41 +0900

    이준희 차량관리 커밋

--- src/main/java/egovframework/carauction/service/impl/CarMngeDAO.java
+++ src/main/java/egovframework/carauction/service/impl/CarMngeDAO.java
@@ -11,7 +11,7 @@ import egovframework.carauction.CarMngeVO;
 
 @Repository("carMngeDAO")
 public class CarMngeDAO extends EgovAbstractMapper {
-	
+
 	// 차량관리 검색조건(제조사) 조회
 	public List<Map<String, Object>> selMkrList(@Param("locImpGb") String locImpGb) {
 		return selectList("carMngeDAO.selMkrList", locImpGb);
@@ -21,7 +21,7 @@ public class CarMngeDAO extends EgovAbstractMapper {
 	public List<CarMngeVO> selSrchCondList(CarMngeVO carMngeVO) {
 		return selectList("carMngeDAO.selSrchCondList", carMngeVO);
 	}
-	
+
 	// 차량관리 검색조건(연료) 조회
 	public List<Map<String, Object>> selFuelList() {
 		return selectList("carMngeDAO.selFuelList");
@@ -36,22 +36,44 @@ public class CarMngeDAO extends EgovAbstractMapper {
 	public CarMngeVO selCarMnge(@Param("carMainNo") String carMainNo) {
 		return selectOne("carMngeDAO.selCarMnge", carMainNo);
 	}
-	
-	
-	
-	
-	// 차량관리 등록
-	public int inscarMnge(Map<String, Object> paramMap) {
-		return insert("carMngeDAO.inscarMnge", paramMap);
-	}
-	
-	// 차량관리 수정
-	public int updcarMnge(Map<String, Object> paramMap) {
-		return update("carMngeDAO.updcarMnge", paramMap);
+
+	// 차량모델명 중복 조회
+	public int selChkCarModelNm(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selChkCarModelNm", carMngeVO);
 	}
 
-	// 차량관리 삭제
-	public int delcarMnge(CarMngeVO CarMngeVO) {
-		return update("carMngeDAO.delcarMnge", CarMngeVO);
+	// 차량모델코드 MAX값 조회
+	public String selMaxCarModelCd(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selMaxCarModelCd", carMngeVO);
+	}
+
+	// 차량등급명 중복 조회
+	public int selChkCarLevelNm(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selChkCarLevelNm", carMngeVO);
+	}
+
+	// 차량등급코드 MAX값 조회
+	public String selMaxCarLevelCd(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selMaxCarLevelCd", carMngeVO);
+	}
+
+	// 차량세부등급명 중복 조회
+	public int selChkCarDtLevelNm(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selChkCarDtLevelNm", carMngeVO);
+	}
+
+	// 차량세부등급코드 MAX값 조회
+	public String selMaxCarDtLevelCd(CarMngeVO carMngeVO) {
+		return selectOne("carMngeDAO.selMaxCarDtLevelCd", carMngeVO);
+	}
+
+	// 차량관리 등록
+	public int insCarMnge(CarMngeVO carMngeVO) {
+		return insert("carMngeDAO.insCarMnge", carMngeVO);
+	}
+
+	// 차량관리 수정
+	public int updCarMnge(CarMngeVO carMngeVO) {
+		return update("carMngeDAO.updCarMnge", carMngeVO);
 	}
 }
